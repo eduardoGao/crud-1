20a5a06247fc850dee0bcdf52d184e6d30abbd7b
Eduardo Diaz <lalo.diaz1896@gmail.com>
2020-12-15 00:02:25 -0600

    Material-Ui

---
+++ src/AppMaterial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@material-ui/core";
+
+function AppMaterial() {
+  return (
+    <div>
+      <Button color='primary' variant='contained' >
+        App Material
+      </Button>
+    </div>
+  )
+}
+
+export default AppMaterial
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppMaterial from './AppMaterial'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppMaterial />
   </React.StrictMode>,
   document.getElementById('root')
 );
